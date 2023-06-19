e6ef55d0c14459fe0703d334ab5eef2136068164
YoelEigner <35924447+YoelEigner@users.noreply.github.com>
2023-06-19 17:29:48 +0300

    added priority algorithm

--- Controllers/taskController.js
+++ Controllers/taskController.js
@@ -1,66 +1,78 @@
-const { ObjectId } = require('mongodb');
-const { mongoClient } = require('../MongoDBConnect');
-const Task = require('../Modals/TaskModal');
+const { ObjectId } = require('mongodb') 
+const { mongoClient } = require('../MongoDBConnect') 
+const Task = require('../Modals/TaskModal') 
 
 // Create a new task
 
-const db = mongoClient.db('tasks');
-const collection = db.collection('task');
+const db = mongoClient.db('tasks') 
+const collection = db.collection('task') 
 
+const calculatePriority = (description) => {
+    let priority = 0
+    const descriptionLength = description.length 
+    if (descriptionLength < 10) {
+        priority = 1
+    } else if (descriptionLength >= 10 && descriptionLength <= 20) {
+        priority = 2
+    } else {
+        priority = 3
+    }
+    return priority
+}
 const createTask = async (req, res) => {
     try {
-        const { title, description, priority } = req.body;
-        const task = new Task(null, title, description, priority);
+        const { title, description } = req.body 
+        let priority = calculatePriority(description)
 
-        const result = await collection.insertOne(task);
-        const createdTask = result.insertedId;
-
-        res.status(201).json(createdTask);
+        const task = new Task(null, title, description, priority) 
+        const result = await collection.insertOne(task) 
+        const createdTask = result.insertedId 
+        res.json({ id: createdTask, priority })
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal server error' });
+        console.error(error) 
+        res.status(500).json({ error: 'Internal server error' }) 
     }
-};
+} 
 const getTaskById = async (req, res) => {
     try {
-        const { id } = req.params;
-        console.log(id);
-
-        const task = await collection.findOne({ _id: new ObjectId(id) });
+        const { id } = req.params 
+        const task = await collection.findOne({ _id: new ObjectId(id) }) 
 
         if (!task) {
-            return res.status(404).json({ error: 'Task not found' });
+            return res.status(404).json({ error: 'Task not found' }) 
         }
-
-        res.json(task);
+        task.id = task._id
+        res.json(task) 
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal server error' });
+        console.error(error) 
+        res.status(500).json({ error: 'Internal server error' }) 
     }
-};
+} 
 
 
 const updateTaskById = async (req, res) => {
     try {
-        const { id } = req.params;
-        const { title, description, priority } = req.body;
+        const { id } = req.params 
+        const { title, description } = req.body 
+        let priority = calculatePriority(description)
+
         const result = await collection.updateOne(
             { _id: new ObjectId(id) },
             { $set: { title, description, priority } }
-        );
+        ) 
         if (result.modifiedCount === 0 && result.matchedCount === 0) {
-            return res.status(404).json({ error: 'Task not found' });
+            return res.status(404).json({ error: 'Task not found' }) 
         }
 
-        res.sendStatus(204);
+        res.json({ id, title, description, priority })
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal server error' });
+        console.error(error) 
+        res.status(500).json({ error: 'Internal server error' }) 
     }
-};
+} 
 
 module.exports = {
     createTask,
     getTaskById,
     updateTaskById,
-};
+} 
